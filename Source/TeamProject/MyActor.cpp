2250ae02530b3af9bcb063a3d5abcdf85fc10059
pro0518 <justmusic0518@gmail.com>
2026-03-18 11:07:19 +0900

    로컬 브랜치 생성 및 커밋
    main 로컬 브랜치를 세이브 포인트로 쓰기 위해 작업 내용은 feature 로컬 브랜치로 따로 파서 진행하는 방법을 배웠다.
    새 로컬 브랜치에서 작업 후 main 로컬 브랜치로 머지하는 연습을 해봤다.

--- Source/TeamProject/MyActor.cpp
+++ Source/TeamProject/MyActor.cpp
@@ -5,7 +5,16 @@
 
 AMyActor::AMyActor(){ PrimaryActorTick.bCanEverTick = true; }
 
-void AMyActor::BeginPlay(){ Super::BeginPlay(); }
+void AMyActor::BeginPlay()
+{
+	Super::BeginPlay();
 
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor has begun play!"));
+}
 
-void AMyActor::Tick(float DeltaTime){ Super::Tick(DeltaTime); }
+void AMyActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UE_LOG(LogTemp, Warning, TEXT("AMyActor is ticking!"));
+}
